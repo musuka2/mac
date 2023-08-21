39572afd20e6b5591cdac179ffbd1a1379525c10
musuka2 <barondoll.j@icloud.com>
2023-08-21 15:02:29 +0900

    ブログ投稿の作成

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,24 +1,23 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <title>Blog</title>
-
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Posts</title>
         <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
-    <body class="antialiased">
-        <h1 class='title'>
+    <body>
+        <h1 class="title">
             {{ $post->title }}
         </h1>
-        <div class='content'>
-            <div class='content_post'>
+        <div class="content">
+            <div class="content__post">
                 <h3>本文</h3>
-                <p class='body'>{{ $post->body }}</p>
+                <p>{{ $post->body }}</p>    
             </div>
         </div>
-        <div class='footer'>
+        <div class="footer">
             <a href="/">戻る</a>
         </div>
     </body>
